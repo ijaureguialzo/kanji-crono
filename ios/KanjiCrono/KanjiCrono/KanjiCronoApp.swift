ec4eeb2c9675bbd5fa5e38d5ca54fdd8e9a91b1f
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2023-04-16 21:03:00 +0200

    Nuevo proyecto de Xcode

---
+++ ios/KanjiCrono/KanjiCrono/KanjiCronoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanjiCronoApp.swift
+//  KanjiCrono
+//
+//  Created by Ion Jaureguialzo Sarasola on 16/4/23.
+//
+
+import SwiftUI
+
+@main
+struct KanjiCronoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
